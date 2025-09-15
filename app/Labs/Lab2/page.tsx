aa5eeac84dec7a70d8eb20fd493f360c69d96791
Jose Annunziato <jannunzi@gmail.com>
2025-09-15 17:14:42 -0400

    fixed quotes

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -10,15 +10,15 @@ export default function Lab2() {
           name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
-          feel
+          Here&apos;s another paragraph using a different ID and a different
+          look and feel
         </p>
       </div>
 
       <h3>Styling with the STYLE attribute</h3>
       <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although it&apos;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
     </div>
