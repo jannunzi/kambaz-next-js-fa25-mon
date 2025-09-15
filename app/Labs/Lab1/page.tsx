542b693ebe22fc6a74ff5ac14fdf6f2e34e9196f
Jose Annunziato <jannunzi@gmail.com>
2025-09-15 17:12:20 -0400

    a1-labs

---
+++ app/Labs/Lab1/page.tsx
@@ -0,0 +1,106 @@
+export default function Lab1() {
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
+      <h3>HTML Examples</h3>
+      <h4>Other HTML field types</h4>
+
+      <label htmlFor="wd-text-fields-salary-start">Starting salary:</label>
+      <input
+        type="number"
+        id="wd-text-fields-salary-start"
+        placeholder="1000"
+        value="100000"
+      />
+      <br />
+
+      <label htmlFor="wd-text-fields-rating">Rating: </label>
+      <input
+        type="range"
+        id="wd-text-fields-rating"
+        placeholder="Doe"
+        max="5"
+        value="4"
+      />
+      <br />
+
+      <div id="wd-forms">
+        <h5 id="wd-checkboxes">Checkboxes</h5>
+        <label>Favorite movie genre:</label>
+        <br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
+        <label htmlFor="wd-chkbox-comedy">Comedy</label>
+        <br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
+        <label htmlFor="wd-chkbox-drama">Drama</label>
+        <br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
+        <label htmlFor="wd-chkbox-scifi">Science Fiction</label>
+        <br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
+        <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+        <h5 id="wd-radio-buttons">Radio buttons</h5>
+        <label>Favorite movie genre:</label>
+        <br />
+        <input type="radio" name="radio-genre" id="wd-radio-comedy" />
+        <label htmlFor="wd-radio-comedy">Comedy</label>
+        <br />
+        <input type="radio" name="radio-genre" id="wd-radio-drama" />
+        <label htmlFor="wd-radio-drama">Drama</label>
+        <br />
+        <input type="radio" name="radio-genre" id="wd-radio-scifi" />
+        <label htmlFor="wd-radio-scifi">Science Fiction</label>
+        <br />
+        <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
+        <label htmlFor="wd-radio-fantasy">Fantasy</label>
+        <h5>Text boxes</h5>
+        <label>Biography:</label>
+        <br />
+        <textarea id="wd-textarea" cols={30} rows={10}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </textarea>
+        <h4>Form Elements</h4>
+        {/* <form id="wd-text-fields"> */}
+        <h5>Text Fields</h5>
+        <label htmlFor="wd-text-fields-username">Username:</label>
+        <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+        <label htmlFor="wd-text-fields-password">Password:</label>
+        <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+        <br />
+        <label htmlFor="wd-text-fields-first-name">First name:</label>
+        <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+        <label htmlFor="wd-text-fields-last-name">Last name:</label>
+        <input
+          type="text"
+          id="wd-text-fields-last-name"
+          placeholder="Doe"
+          value="Wonderland"
+          title="The last name"
+        />
+        {/* copy rest of form elements here  */}
+        {/* </form> */}
+      </div>
+
+      <div id="wd-h-tag">
+        <h4>Heading Tags</h4>
+        Text documents are often broken up into several sections and
+        subsections. Each section is usually prefaced with a short title or
+        heading that attempts to summarize the topic of the section it precedes.
+        For instance this paragraph is preceded by the heading Heading Tags. The
+        font of the section headings are usually larger and bolder than their
+        subsection headings. This document uses headings to introduce topics
+        such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags
+        can be used to format plain text so that it renders in a browser as
+        large headings. There are 6 heading tags for different sizes: h1, h2,
+        h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest
+        heading.
+      </div>
+    </div>
+  );
+}
